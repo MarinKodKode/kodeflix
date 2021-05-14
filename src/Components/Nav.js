ef06ff56d66d979bf05ae1cf31987d4c1d758c49
MarinKodKode <72593577+MarinKodKode@users.noreply.github.com>
2021-05-14 12:02:16 -0500

    First commit

---
+++ src/Components/Nav.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://logos-marcas.com/wp-content/uploads/2020/04/Netflix-Logo-650x366.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav_avatar"
+        src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+        alt="Avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
